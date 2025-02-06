34644c68c64a9cdb0089e45caa6a8ca503af2b98
joshi-jatin <thegreedybat27@gmail.com>
2025-02-06 23:47:29 +0530

    Update orderController.js

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user model for frontend
 const placeOrder = async (req,res)=>{
 
-    const frontend_url = "http://localhost:3000";
+    const frontend_url = "https://food-del-frontend-sutt.onrender.com/";
 
     try {
         const newOrder = new orderModel({
