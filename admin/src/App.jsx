df34516d6a641aec0bd504c11a6dbbd271cb8827
joshi-jatin <thegreedybat27@gmail.com>
2025-02-06 23:56:40 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-9e7j.onrender.com";
 
   return (
     <div>
